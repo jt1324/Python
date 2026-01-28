614c253f09ac94012c01258343615ce5bd86c707
Jean <jeantoneli@gmail.com>
2026-01-28 22:36:25 +0000

    ex033 done

---
+++ Exercises/ex033.py
@@ -0,0 +1,10 @@
+n1 = int(input('Type the first number: '))
+n2 = int(input('Type the second number: '))
+n3 = int(input('Type the third number: '))
+gro = [n1, n2, n3]
+
+max = max(gro)
+min = min(gro)
+
+print('The higher number is {}.'.format(max))
+print('The lowest number is {}.'.format(min))
