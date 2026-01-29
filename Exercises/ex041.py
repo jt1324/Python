6a3e107ef33fb4014ebef4d774883d3c15f39c98
Jean <jeantoneli@gmail.com>
2026-01-29 23:35:49 +0000

    ex041 done

---
+++ Exercises/ex041.py
@@ -0,0 +1,13 @@
+yea = int(input('Whar year did you born? '))
+age = 2026-yea
+
+if age <=9:
+  print ('Your category is MIRIM.')
+elif age <=14:
+  print ('Your category is INFANT.')
+elif age <=19:
+  print ('Your category is JUNIOR.')
+elif age <=50:
+  print ('Your category is SENIOR.')
+else:
+  print ('Your category is MASTER')
