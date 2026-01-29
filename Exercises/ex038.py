67136235ebc606d0ca65f5f45ce3fbe8673d9cf9
Jean <jeantoneli@gmail.com>
2026-01-29 23:15:49 +0000

    ex038 done

---
+++ Exercises/ex038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Type the first number: '))
+n2 = int(input('Type the second mumber: '))
+
+if n1 > n2:
+  print ('The first mumber is higher.')
+elif n2 > n1:
+  print ('The second munber is higher.')
+else:
+  print ('The numbers are the same.')
