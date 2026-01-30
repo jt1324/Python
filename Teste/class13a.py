0af21675bae469e166c5df94be0237b76e16c003
Jean <jeantoneli@gmail.com>
2026-01-30 00:57:57 +0000

    class13 done

---
+++ Teste/class13a.py
@@ -0,0 +1,26 @@
+for c in range (1, 5):
+  print('Hi')
+print('end')
+
+for c in range (1, 6):
+  print (c)
+print('end')
+
+for c in range (6, 0, -1):
+  print (c)
+print('end')
+
+num = int(input('Add a number: '))
+for any in range(0, num+1):
+  print(any)
+print('end')
+
+for som in range(0, 3):
+  n = int(input('Type a number: '))
+print('end')
+
+s = 0
+for sum in range(0, 3):
+  n = int(input('Type a number: '))
+  s += n
+print ('The sum is {}.'.format(s))
