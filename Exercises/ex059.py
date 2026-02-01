6c83632212cf236a8b42ace25e37c2fcc7450b69
Jean <jeantoneli@gmail.com>
2026-02-01 23:49:21 +0000

    ex 059 new improvmants

--- Exercises/ex059.py
+++ Exercises/ex059.py
@@ -1,58 +1,24 @@
 n1 = int(input('Add a number: '))
 n2 = int(input('Add other nuber: '))
-print ('''
-       [1] Sum
-       [2]Multiply
-       [3]Higher
-       [4]New numbers
-       [5]Leave''')
-opt = int(input('Chose one options: '))
+opt = 0
 
 while opt != 5:
+  print ('''
+       [1] Sum
+       [2] Multiply
+       [3] Higher
+       [4] New numbers
+       [5] Leave''')
+  opt = int(input('Chose one options: '))
   if opt not in (1, 2, 3, 4):
     print('Whrong number, chose one of the below.')
-    print ('''
-       [1] Sum
-       [2]Multiply
-       [3]Higher
-       [4]New numbers
-       [5]Leave''')
-    opt = int(input('Chose one options: '))
   if opt == 1:
     print('The sum of {} and {} is {}.'.format(n1, n2, n1+n2))
-    print ('''
-       [1] Sum
-       [2]Multiply
-       [3]Higher
-       [4]New numbers
-       [5]Leave''')
-    opt = int(input('Chose one options: ')) 
   if opt == 2:
     print('The multiplication of {} for {} is {}.'.format(n1, n2, n1*n2))
-    print ('''
-       [1] Sum
-       [2]Multiply
-       [3]Higher
-       [4]New numbers
-       [5]Leave''')
-    opt = int(input('Chose one options: '))
   if opt == 3:
     print('The higher value between {} and {} is {}.'.format(n1, n2, max(n1, n2)))
-    print ('''
-       [1] Sum
-       [2]Multiply
-       [3]Higher
-       [4]New numbers
-       [5]Leave''')
-    opt = int(input('Chose one options: '))
   if opt == 4:
     n1 = int(input('No problem, type the new first number: '))
     n2 = int(input('Type the new second number again: '))
-    print ('''
-       [1] Sum
-       [2]Multiply
-       [3]Higher
-       [4]New numbers
-       [5]Leave''')
-    opt = int(input('Chose one options: '))
 print('You are leaving the program, bye!')
