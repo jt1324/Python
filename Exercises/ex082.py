66db8d53a7cf5f04208ac7381d49bc700f3b61b6
Jean <jeantoneli@gmail.com>
2026-02-05 22:45:43 +0000

    ex082 done

---
+++ Exercises/ex082.py
@@ -0,0 +1,20 @@
+l1 = []
+le = []
+lo = []
+
+while True:
+  n = int(input('Type a number: '))
+  l1.append(n)
+  c = str(input('Do you want to continue? [S/N]: ')).strip().upper()[0]
+  if c == 'N':
+    break
+print (f'The complete list is {l1}')
+
+for l in l1:
+  if l % 2 == 0:
+    le.append(l)
+  else:
+    lo.append(l)
+
+print (f'The EVEN list is {le}.')
+print (f'The ODD list is {lo}.')
