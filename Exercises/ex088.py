ca82c27f1013234821a5b017e37f2871bfb1f09e
Jean <jeantoneli@gmail.com>
2026-02-09 23:36:41 +0000

    ex088 adding sleep

--- Exercises/ex088.py
+++ Exercises/ex088.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep
 
 print ('--'*15)
 print ('      EURO MILLION GAME')
@@ -12,5 +13,6 @@ print (f"{'-='*5}  Getting numbers for {pla} games  {'=-'*5}")
 for r in range(0, pla):
   num = (random.randint(0, 60), random.randint(0, 60), random.randint(0, 60), random.randint(0, 60), random.randint(0, 60), random.randint(0, 60))
   print(f'Game {c} : {sorted(num)}')
+  sleep(1)
   c += 1
 print (f"{'=='*5} < GOOD LUCK > {'=='*5}")
