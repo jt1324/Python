867ee093696a4a4a0fe5b750e310d308fce28cc2
Jean <jeantoneli@gmail.com>
2026-02-11 21:38:01 +0000

    ex092 done

---
+++ Exercises/ex092.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+data = {}
+
+data['name'] = str(input('Name: '))
+data['age'] = date.today().year - int(input('Birthday year: '))
+data['NIN'] = int(input("N.I.N. (0 if doesn't have): "))
+if data['NIN'] == 0:
+  print ('=-'*20)
+  print (data)
+  print (f'Name is: {data['name']}')
+  print (f'The age is: {data['age']}')
+  print ("Doesn't have a NIN")
+else:
+  data['year'] = int(input('Year of start working: '))
+  data['salary'] = float(input('Salary: £'))
+  print ('=-'*20)
+  print (data)
+  print (f'Name is: {data['name']}')
+  print (f'The age is: {data['age']}')
+  print (f'NIN is: {data['NIN']}')
+  print (f'Start working year: {data['year']}')
+  print (f'The salary is: £{data['salary']}')
+  print (f'Will retire at the age {(data['year']+35)-(date.today().year - data['age'])}')
+print ('=-'*20)
