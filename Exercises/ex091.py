1c8bd65ac978a0d7fc141d90f2208ad72a5d6213
Jean <jeantoneli@gmail.com>
2026-02-11 21:00:21 +0000

    ex091 done + video solution

--- Exercises/ex091.py
+++ Exercises/ex091.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 players = dict()
 game = list()
@@ -7,13 +8,41 @@ c = 1
 for p in range(0, 4):
   players[(f'Player{c}')] = randint(1, 6)
   c += 1
-print (players)
 
-sorted = dict(sorted(players.items(), key=lambda item: item[1], reverse=True))
+o = 1
+for p in players:
+  print(f"{p} got {players[p]}")
+  o +=1
+  sleep(1)
+  
+sort = dict(sorted(players.items(), key=lambda item: item[1], reverse=True))
 
-print (sorted)
-
+print (' xxxx Players Ranking! xxxx ')
 ord = 1
-for p in sorted:
-  print(f"{ord} place: {p} with {sorted[p]}")
+for p in sort:
+  print(f"{ord} place: {p} with {sort[p]}")
   ord +=1
+  sleep(1)
+
+
+# ----------------------------------------------------------
+# VIDEO SOLUTION
+
+from operator import itemgetter
+
+jogo = {'player1': randint(1, 6),
+        'player2': randint(1, 6),
+        'player3': randint(1, 6),
+        'player4': randint(1, 6)}
+rank = list()
+print('Numbers generated:')
+for k, v in jogo.items():
+  print (f'{k} got {v}')
+  sleep(1)
+rank = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print('=+'*20)
+print('       == RANKING ==  ')
+for i, v in enumerate(rank):
+  print (f' {i+1}o place: {v[0]} with {v[1]}.')
+  sleep(1)
+  
