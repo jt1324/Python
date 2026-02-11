ea243794c3fb4baf24f0488f849b7e302fc7ac54
Jean <jeantoneli@gmail.com>
2026-02-11 22:32:29 +0000

    ex093 improvments

--- Exercises/ex093.py
+++ Exercises/ex093.py
@@ -3,18 +3,13 @@ goals = []
 
 info['name'] = str(input("Player's name: "))
 gam = int(input(f'How many games {info['name']} played? '))
-c = 1
-t = 0
+
 for i in range(0, gam):
-  g = int(input(f'How many goals he scored on game {c}? '))
+  g = int(input(f'How many goals he scored on game {i+1}? '))
   goals.append(g)
-  c += 1
-  if t == 0:
-    t = g
-  else:
-    t += g
-info['goals'] = goals
-info['total'] = t
+  
+info['goals'] = goals[:]
+info['total'] = sum(goals)
 print ('=-'*20)
 print (info)
 print ('=-'*20)
@@ -23,10 +18,7 @@ for k, v in info.items():
   print (f' -The info {k} has the value {v}.')
 print ('=-'*20)
 print (f'The player {info['name']} played {gam} games.')
-s = 0
-p = 1
-for v in info['goals']:
-  print (f' => In the game {p}, scored {info['goals'][s]} goals.')
-  p += 1
-  s += 1
+
+for i, v in enumerate(info['goals']):
+  print (f' => In the game {i+1}, scored {v} goals.')
 print (f'Was a total of {info['total']} goals.')
