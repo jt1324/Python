a1b7dc719637e2efef9378cdbff90ab1bc863c1d
Jean <jeantoneli@gmail.com>
2026-02-17 21:11:14 +0000

    ex100 adding teacher's result

--- Exercises/ex100.py
+++ Exercises/ex100.py
@@ -1,25 +1,46 @@
 from random import randint
 from time import sleep
 
-numbers = list()
-# su = 0
+# numbers = list()
+# # su = 0
 
-def sort (*num):
-  su = 0
+# def sort (*num):
+#   su = 0
+#   print ('Getting 5 numbers from the list: ', end='')
+#   for n in num:
+#     numbers.append(n)
+#     print (f'{n} ', end='', flush=True)
+#     sleep(0.25)
+#     if n % 2 == 0:
+#       su += n
+
+#   print ('DONE!')
+#   print (f'The sum of EVEN numbers of {numbers} is {su}.')
+
+
+# sort (randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9))
+
+# teacher's solution
+
+def sort(list):
   print ('Getting 5 numbers from the list: ', end='')
+  for nu in range(0, 5):
+    n = randint(1, 10)
+    list.append(n)
+    print (f'{n} ', end='', flush = True)
+    sleep(0.3)
+  print('Ready!')
+
+
+def even(num):
+  ev = 0
   for n in num:
-    numbers.append(n)
-    print (f'{n} ', end='', flush=True)
-    sleep(0.25)
     if n % 2 == 0:
-      su += n
+      ev += n
+  print (f'The sum of EVEN numbers of {numbers} is {ev}.')
 
-  print ('DONE!')
-  print (f'The sum of EVEN numbers of {numbers} is {su}.')
-
-
-sort (randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9))
-
-
+numbers = list()
+sort(numbers)
+even(numbers)
 
 
