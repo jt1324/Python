daa1c42941e7b521c6c689dc671c203d51c2c6ab
Jean <jeantoneli@gmail.com>
2026-02-17 22:57:51 +0000

    ex102 adding Docstring

--- Exercises/ex102.py
+++ Exercises/ex102.py
@@ -1,4 +1,11 @@
 def fact(n, show=False):
+  """
+  Docstring for fact
+  
+  :param n: Start of factorial calculation
+  :param show: Show or not the details of the calculation
+  :return: The value of factorial n
+  """
   f = 1
   for c in range(n, 0, -1):
     if show:
