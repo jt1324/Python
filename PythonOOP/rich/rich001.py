172cc0666516c93ddb756dd8d1a87a1067813205
Jean <jeantoneli@gmail.com>
2026-02-25 22:22:56 +0000

    rich001 done

---
+++ PythonOOP/rich/rich001.py
@@ -0,0 +1,8 @@
+from rich import print
+
+print('hello world!')
+print('Hellow [red]world[/red]! :earth_americas:')
+print('Ola, [bold blue]Pequeno Gafanhoto[/] :vulcan_salute:')
+print('Ola, [bold blue on white]Pequeno Gafanhoto[/] :vulcan_salute:')
+print(':+1: :-1:')
+print(':zap: :zany_face: :scissors: :yum:')
