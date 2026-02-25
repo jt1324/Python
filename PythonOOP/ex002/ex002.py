d4a127d74b2372a2fa46a7ba457ea0b6848d93fd
Jean <jeantoneli@gmail.com>
2026-02-25 21:26:51 +0000

    ex002 done

--- PythonOOP/ex002/ex002.py
+++ PythonOOP/ex002/ex002.py
@@ -19,19 +19,21 @@ class MyClass:
   def __str__(self):   # Dunder Method
     return (f'Hello, my name is {self.name} and I am {self.age} years old.')
 
+  def __getstate__(self):
+    return f"State: Name = {self.name}; Age = {self.age}"
 
 
 
 # declare an object
-person = MyClass('Jean', 28)  # create an instance of MyClass and assign it to the variable   person
-person.birthday()             # call the birthday method of the person object, which increments the age by 1
-# print(person.message()) 
-
+person = MyClass('Jean', 28)  
+person.birthday()             
 print(person)
-# person1 = MyClass('Maria', 30)
-# print(person1.message())
-
-# person2 = MyClass()
-# print(person2.message())
 
 print(person.__doc__)
+print(person.__dict__)        # Attribute
+print(person.__getstate__())  # Method
+print(person.__class__)
+
+person1 = MyClass('Maria', 30)
+print(person1)
+print(person1.__getstate__())
