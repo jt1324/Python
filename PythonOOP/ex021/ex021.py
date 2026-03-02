604b686dbef5062e644b5d6ccacbb19dc7ba64e6
Jean <jeantoneli@gmail.com>
2026-03-02 22:03:04 +0000

    ex021 uncover part

--- PythonOOP/ex021/ex021.py
+++ PythonOOP/ex021/ex021.py
@@ -1,12 +1,19 @@
 from rich import print
 
 class Pen:
-  def __init__(self, color):
+  def __init__(self, color, cover=False):
     self.color = color
+    self.cover = False
     
+  def uncover(self):
+    self.cover = True
+  
   def write(self, text):
-    print (f"[{self.color}]{text}[/]")
-
+    if self.cover == True:
+      print (f"[{self.color}]{text}[/]")
+    else:
+      print (f"The [{self.color}]pen[/] is covered!")
+  
   def jump(self, lines):
     if lines == 1:
       print('\n')
@@ -14,9 +21,14 @@ class Pen:
       for _ in range(lines-1):
         print('\n')
 
+
 c1 = Pen('blue')
 c2 = Pen('red')
 c3 = Pen('green')
+
+c1.uncover()
+c2.uncover()
+# c3.uncover()
 
 c1.write("Hi, are you ok?")
 c1.jump(2)
