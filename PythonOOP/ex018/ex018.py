351400f99f298ac45b7aa6f9379a36213ea8ddb5
Jean <jeantoneli@gmail.com>
2026-03-03 21:43:06 +0000

    ex018 - improv - adding values as class atributes

--- PythonOOP/ex018/ex018.py
+++ PythonOOP/ex018/ex018.py
@@ -5,13 +5,15 @@ install()
 
 
 class Check:
+  weight_per_person = 0.4
+  price_per_kg = 25.00
 
   def __init__(self, title, qty):
     self.title = title
     self.qty = qty
 
   def bbq(self):
-    return Panel(f"Checking [green]{self.title}[/] with [blue]{self.qty} people[/]\nEach person will eat 0.4Kg and the Kg cost is £25.00\nRecomended buy [blue]{self.qty * 0.4}[/]Kg of meat\nThe total cost will be [green]£{(self.qty * 0.4)*25:,.2f}[/]\nEach person will pay [yellow]£{((self.qty * 0.4)*25)/self.qty:,.2f}[/] to join.", title=f'{self.title}')
+    return Panel(f"Checking [green]{self.title}[/] with [blue]{self.qty} people[/]\nEach person will eat {self.weight_per_person}Kg and the Kg cost is £{self.price_per_kg:,.2f}\nRecomended buy [blue]{self.qty * self.weight_per_person}[/]Kg of meat\nThe total cost will be [green]£{((self.qty * self.weight_per_person) * self.price_per_kg):,.2f}[/]\nEach person will pay [yellow]£{(((self.qty * self.weight_per_person) * self.price_per_kg) / self.qty):,.2f}[/] to join.", title=f'{self.title}')
   
 
 bbq1 = Check('Friends BBQ', 50)
