26ee539d761093d42ef3cd1d350ac55fa5920964
Jean <jeantoneli@gmail.com>
2026-07-29 23:36:45 +0100

    creating superclass and subclasses

--- PythonOOP/ex004/ex004.py
+++ PythonOOP/ex004/ex004.py
@@ -1,7 +1,7 @@
 from ast import Pass
 
 
-class People:
+class People:                                   # Base class | Superclass | Parent class | Class Base | Class Mother
     def __init__(self, name = "", age = 0):
         self.name = name
         self.age = age
@@ -9,12 +9,32 @@ class People:
     def birthday(self):
         self.age += 1
 
-class Student:
-    pass
 
-class Teacher:
-    pass
+class Student(People):                           # Derived class | Subclass | Child class | Class Derived | Class Son
+    def __init__ (self, name, age, grade, classroom):
+        super().__init__(name, age)
+        self.grade = ""
+        self.classroom = ""
+    
+    def enroll(self):
+        pass
 
-class Employee:
-    pass
 
+class Teacher(People):                           # Derived class | Subclass | Child class | Class Derived | Class Son
+    def __init__(self, name, age, especialty, level):
+        super().__init__(name, age)
+        self.especialty = ""
+        self.level = ""
+
+    def teach(self):
+        pass
+
+
+class Employee(People):                           # Derived class | Subclass | Child class | Class Derived | Class Son  
+    def __init__(self, name, age, position, sector):
+        super().__init__(name, age)
+        self.position = ""
+        self.sector = ""
+
+    def work(self):
+        pass
