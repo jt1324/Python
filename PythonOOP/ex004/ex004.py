f880fb67cdec0b38efb4f692c564db56f869df24
Jean <jeantoneli@gmail.com>
2026-07-29 23:54:51 +0100

    nearly finished

--- PythonOOP/ex004/ex004.py
+++ PythonOOP/ex004/ex004.py
@@ -1,4 +1,5 @@
 from ast import Pass
+from rich import print, inspect
 
 
 class People:                                   # Base class | Superclass | Parent class | Class Base | Class Mother
@@ -13,28 +14,45 @@ class People:                                   # Base class | Superclass | Pare
 class Student(People):                           # Derived class | Subclass | Child class | Class Derived | Class Son
     def __init__ (self, name, age, grade, classroom):
         super().__init__(name, age)
-        self.grade = ""
-        self.classroom = ""
+        self.grade = grade
+        self.classroom = classroom
     
     def enroll(self):
-        pass
+        print(f"{self.name} is enrolled in {self.classroom} grade {self.grade}")
 
 
 class Teacher(People):                           # Derived class | Subclass | Child class | Class Derived | Class Son
     def __init__(self, name, age, especialty, level):
         super().__init__(name, age)
-        self.especialty = ""
-        self.level = ""
+        self.especialty = especialty
+        self.level = level
 
     def teach(self):
-        pass
+        print(f"{self.name} is teaching {self.especialty} at {self.level} level")
 
 
 class Employee(People):                           # Derived class | Subclass | Child class | Class Derived | Class Son  
     def __init__(self, name, age, position, sector):
         super().__init__(name, age)
-        self.position = ""
-        self.sector = ""
+        self.position = position
+        self.sector = sector
 
     def work(self):
-        pass
+        print(f"{self.name} is working as {self.position} in the {self.sector} sector")
+
+
+
+s1 = Student("John", 20, "IT", "1A")
+s1.birthday()
+s1.enroll()
+inspect(s1, methods=True)
+
+t1 = Teacher("Jane", 30, "Math", "Master")
+t1.birthday()
+t1.teach()
+inspect(t1, methods=True)
+
+e1 = Employee("Jim", 40, "Manager", "HR")
+e1.birthday()
+e1.work()
+inspect(e1, methods=True)
