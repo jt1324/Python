ed185fa03c1712eb7a2dcf164da3d27822d4dd7c
Jean <jeantoneli@gmail.com>
2026-07-29 23:24:46 +0100

    set up ex004

---
+++ PythonOOP/ex004/ex004.py
@@ -0,0 +1,20 @@
+from ast import Pass
+
+
+class People:
+    def __init__(self, name = "", age = 0):
+        self.name = name
+        self.age = age
+    
+    def birthday(self):
+        self.age += 1
+
+class Student:
+    pass
+
+class Teacher:
+    pass
+
+class Employee:
+    pass
+
