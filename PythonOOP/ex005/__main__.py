9080445134122c84752ee89fe60a90ce6f58efc6
Jean <jeantoneli@gmail.com>
2026-07-30 22:52:04 +0100

    ex005 final

--- PythonOOP/ex005/__main__.py
+++ PythonOOP/ex005/__main__.py
@@ -9,14 +9,14 @@ from class_ex_005 import Student, Teacher, Employee
 s1 = Student("John", 20, "IT", "1A")
 s1.birthday()
 s1.enroll()
-inspect(s1, methods=True)
+#inspect(s1, methods=True)
 
 t1 = Teacher("Jane", 30, "Math", "Master")
 t1.birthday()
 t1.teach()
-inspect(t1, methods=True)
+#inspect(t1, methods=True)
 
 e1 = Employee("Jim", 40, "Manager", "HR")
 e1.birthday()
 e1.work()
-inspect(e1, methods=True)
+#inspect(e1, methods=True)
