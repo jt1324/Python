2255f66da51e1f3d264b1375812dfecf1359b589
Jean <jeantoneli@gmail.com>
2026-07-30 22:54:23 +0100

    ex005 final final

--- PythonOOP/ex005/__main__.py
+++ PythonOOP/ex005/__main__.py
@@ -5,18 +5,21 @@ from class_ex_005 import Student, Teacher, Employee
 
 
 
+def main():
+    s1 = Student("John", 20, "IT", "1A")
+    s1.birthday()
+    s1.enroll()
+    #inspect(s1, methods=True)
 
-s1 = Student("John", 20, "IT", "1A")
-s1.birthday()
-s1.enroll()
-#inspect(s1, methods=True)
+    t1 = Teacher("Jane", 30, "Math", "Master")
+    t1.birthday()
+    t1.teach()
+    #inspect(t1, methods=True)
 
-t1 = Teacher("Jane", 30, "Math", "Master")
-t1.birthday()
-t1.teach()
-#inspect(t1, methods=True)
+    e1 = Employee("Jim", 40, "Manager", "HR")
+    e1.birthday()
+    e1.work()
+    #inspect(e1, methods=True)
 
-e1 = Employee("Jim", 40, "Manager", "HR")
-e1.birthday()
-e1.work()
-#inspect(e1, methods=True)
+if __name__ == "__main__":
+    main()
