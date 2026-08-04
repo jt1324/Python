2715a16ab0ed4baf61c456a6a6c4f0591ba655f2
Jean <jeantoneli@gmail.com>
2026-08-04 23:18:07 +0100

    ex026 almost done

--- PythonOOP/ex026/employees.py
+++ PythonOOP/ex026/employees.py
@@ -2,12 +2,13 @@ from rich.panel import Panel
 from rich import print
 from abc import ABC, abstractmethod
 
+
 class Employee(ABC):
     def __init__(self, name, gross_salary, salary, min_wage, nin):
         self.name = name
-        self.gorss_salary = gross_salary
+        self.gross_salary = gross_salary
         self.salary = salary
-        self.nim_wage = min_wage
+        self.min_wage = min_wage
         self.nin = nin
 
     @abstractmethod
@@ -18,23 +19,30 @@ class Employee(ABC):
         pass
 
 
-class Hourly(Employee):
+class EmployeeHour(Employee):
     def __init__(self, name, value_hour, hours_worked):
-        self.name = name
+        super().__init__(name, 0, 0, 0, 0)
         self.value_hour = value_hour
         self.hours_worked = hours_worked
 
     def calc_salary(self):
-        pass
+        self.nin = 0.20
+        self.min_wage = 1800
+        self.salary = (self.value_hour * self.hours_worked) * (1 - self.nin)
+
+    def analyse_salary(self):
+        print(f"{self.name}'s salary (Hourly Employee) is £{self.salary} and correspond to {self.salary/self.min_wage:.1f} minimum wages.")
 
 
-class Monthly(Employee):
-    def __init__(self, name, nim_wage, nin):
-        self.name = name
-        self.min_wage = min_wage
-        self.nin = nin
+class EmployeeMonth(Employee):
+    def __init__(self, name, gross_salary):
+        super().__init__(name, 0, 0, 0, 0)
+        self.gross_salary = gross_salary
 
     def calc_salary(self):
-        pass
+        self.nin = 0.20
+        self.min_wage = 1800
+        self.salary =  self.gross_salary * (1 - self.nin)
 
-
+    def analyse_salary(self):
+        print(f"{self.name}'s salary (Monthly Employee) is £{self.salary} and correspond to {self.salary/self.min_wage:.1f} minimum wages.")
