744089662549536e87df3049868d2508b455cbb9
Jean <jeantoneli@gmail.com>
2026-08-05 22:15:33 +0100

    adding print details - still before AI

--- PythonOOP/ex027/persona_rpg.py
+++ PythonOOP/ex027/persona_rpg.py
@@ -4,16 +4,24 @@ from random import randint
 from abc import ABC
 
 class Persona(ABC):
-    def __init__(self, name, live, blow):
+    def __init__(self, name, live, blow = 0, target, power, damage):
         self.name = name
         self.live = live
-        self.blow = blow
+        self.blow = randint(1, self.power)
+        self.target = target
+        self.power = power
+        self.damage = damage
     
     def attack(self, target, power):
-        pass
+        print(f"[green]{Warrior(self.name)}[/green]([light_green]{self.live}[/light_green]) attacked 
+            [red]{Wizard(self.name)}[/red]([light_green]{Wizard(self.live)}[]/light_green) with a [blue]Upercut[/blue] of power 
+            [light_green]{self.power}[/light_green].") 
+        print(f"[blue]{Wizard(self.name)}[/blue] got [red]damage of {dem}[/red]!")
+
 
     def take_demage(self, demage):
-        pass
+        dem = randint(1, self.demage)
+
 
     @abstractmethod
     def heal(self):
@@ -25,7 +33,7 @@ class Warrior():
         pass
     
     def heal(self):
-        pass
+        print(f"[blue]{self.name}[/blue] wrapped a bandage around the wounds and [green]recovered {randint(1, 100)} points[/green] of live.")
 
     
 class Wizard():
@@ -33,5 +41,5 @@ class Wizard():
         pass
 
     def heal(self):
-        pass
+        print(f"[blue]{self.name}[/blue] worked some healing magic and [green]recovered {randint(1, 100)} points[/green] of live.")
 
