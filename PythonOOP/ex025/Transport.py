f47a693f0851c7f1f8a439d539eceee49a77f42a
Jean <jeantoneli@gmail.com>
2026-08-05 23:04:09 +0100

    ex025 - small changes to match solution

--- PythonOOP/ex025/Transport.py
+++ PythonOOP/ex025/Transport.py
@@ -1,39 +1,42 @@
 from abc import ABC, abstractmethod
 
 class Transport(ABC):
-    def __init__(self, distance, cost_per_km):
+    def __init__(self, distance):
         self.distance = distance
-        self.cost_per_km = cost_per_km
+        self.cost_per_km = 0
 
     @abstractmethod
     def cost(self):
         pass
 
 class Motorcycle(Transport):
+    factor = 0.5
     def __init__(self, distance):
-        super().__init__(distance, 0.5)
+        super().__init__(distance)
 
     def cost(self):
-        return self.distance * self.cost_per_km
+        return self.distance * Motorcycle.factor
 
 class Lorry(Transport):
+    factor = 1.2
     def __init__(self, distance):
-        super().__init__(distance, 1.2)
+        super().__init__(distance)
 
     def cost(self):
         if self.distance < 50:
             print("Lorry is not available for distances less than 50 km")
             return 0
         else:
-            return self.distance * self.cost_per_km
+            return self.distance * Lorry.factor
 
 class Drone(Transport):
+    factor = 9.5
     def __init__(self, distance):
-        super().__init__(distance, 9.5)
+        super().__init__(distance)
 
     def cost(self):
         if self.distance > 10:
             print("Drone is not available for distances greater than 10 km")
             return 0
         else:
-            return self.distance * self.cost_per_km
+            return self.distance * Drone.factor
