7d6fcef29d68b6b0e11fb4399d22b0bec2f20c72
Jean <jeantoneli@gmail.com>
2026-08-05 21:28:20 +0100

    setting up ex027

---
+++ PythonOOP/ex027/__main__.py
@@ -0,0 +1,13 @@
+from persona_rpg import *
+
+def man():
+    p1 = Warrior("Kratos", 2000)
+    p2 = Wizard("Merlin", 3000)
+
+    p1.attack(p2, 1000)
+    p2.heal()
+    p2.attack(p1, 20000)
+    p1.heal()
+
+if __name__ == "__main__":
+    main()
