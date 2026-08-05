4b101df7aa7992f31a3250b7a12d85ca4deb94ae
Jean <jeantoneli@gmail.com>
2026-08-05 23:33:22 +0100

    ex026 - changes to match solution

--- PythonOOP/ex026/employees.py
+++ PythonOOP/ex026/employees.py
@@ -4,19 +4,23 @@ from abc import ABC, abstractmethod
 
 
 class Employee(ABC):
-    def __init__(self, name, gross_salary=0, salary=0, min_wage=0, nin=0):
+    
+    min_wage = 1800
+    nin = 0.20
+
+    def __init__(self, name = None):
         self.name = name
-        self.gross_salary = gross_salary
-        self.salary = salary
-        self.min_wage = 1800
-        self.nin = 0.20
+        self.gross_salary = 0
+        self.salary = 0
+       
 
     @abstractmethod
     def calc_salary(self):
         pass
 
     def analyse_salary(self):
-        pass
+        panel = Panel(f"[blue]{self.name}'s[/blue] salary [magenta]({self.__class__.__name__})[/] is [green]£{self.salary:.2f}[/green] and correspond to [yellow]{self.salary/Employee.min_wage:.1f} minimum wages[/yellow].", title = "Salary Analysis", width = 50, expand = False, border_style = "light_green")
+        print(panel)
 
 
 class EmployeeHour(Employee):
@@ -26,21 +30,21 @@ class EmployeeHour(Employee):
         self.hours_worked = hours_worked
 
     def calc_salary(self):
-        self.salary = (self.value_hour * self.hours_worked) * (1 - self.nin)
+        self.salary = (self.value_hour * self.hours_worked) * (1 - Employee.nin)
 
-    def analyse_salary(self):
-        panel = Panel(f"[blue]{self.name}'s[/blue] salary [magenta](Hourly Employee)[/magenta] is [green]£{self.salary:.2f}[/green] and correspond to [yellow]{self.salary/self.min_wage:.1f} minimum wages[/yellow].", title = "Salary Analysis", width = 50, expand = False, border_style = "light_green")
-        print(panel)
+    # def analyse_salary(self):
+    #     panel = Panel(f"[blue]{self.name}'s[/blue] salary [magenta](Hourly Employee)[/magenta] is [green]£{self.salary:.2f}[/green] and correspond to [yellow]{self.salary/Employee.min_wage:.1f} minimum wages[/yellow].", title = "Salary Analysis", width = 50, expand = False, border_style = "light_green")
+    #     print(panel)
 
 
 class EmployeeMonth(Employee):
-    def __init__(self, name, gross_salary):
+    def __init__(self, name, gross_salary = Employee.min_wage):
         super().__init__(name)
         self.gross_salary = gross_salary
 
     def calc_salary(self):
-        self.salary =  self.gross_salary * (1 - self.nin)
+        self.salary =  self.gross_salary * (1 - Employee.nin)
 
-    def analyse_salary(self):
-        panel = Panel(f"[blue]{self.name}'s[/blue] salary [magenta](Monthly Employee)[/magenta] is [green]£{self.salary:.2f}[/green] and correspond to [yellow]{self.salary/self.min_wage:.1f} minimum wages[/yellow].", title = "Salary Analysis", width = 50, expand = False, border_style = "light_green")
-        print(panel)
+    # def analyse_salary(self):
+    #     panel = Panel(f"[blue]{self.name}'s[/blue] salary [magenta](Monthly Employee)[/magenta] is [green]£{self.salary:.2f}[/green] and correspond to [yellow]{self.salary/Employee.min_wage:.1f} minimum wages[/yellow].", title = "Salary Analysis", width = 50, expand = False, border_style = "light_green")
+    #     print(panel)
