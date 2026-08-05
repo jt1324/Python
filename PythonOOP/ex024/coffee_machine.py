9474965838c76a9672ad14bdbed36f8e3b984a83
Jean <jeantoneli@gmail.com>
2026-08-05 22:46:15 +0100

    ex024 - only spaces, al lthe same as solituin

--- PythonOOP/ex024/coffee_machine.py
+++ PythonOOP/ex024/coffee_machine.py
@@ -2,13 +2,13 @@ from abc import ABC, abstractmethod
 from rich import print
 
 class HotDrink(ABC):
+    
     def prepare(self):
         print("--- Starting the preparation ---")
         self.boil()
         self.mix()
         self.serving()
         self.serve()
-
 
     def boil(self):
         print("1. Boil the water at 100°C")
@@ -25,6 +25,7 @@ class HotDrink(ABC):
         print("--- The drink is ready ---")
 
 class Coffee(HotDrink):
+    
     def mix(self):
         print(f"2. Pass the pressured water through the ground coffee powder")
 
@@ -33,6 +34,7 @@ class Coffee(HotDrink):
 
 
 class Tea(HotDrink):
+    
     def mix(self):
         print(f"2. Put the chosen tea bag in the water and let it brew")
 
@@ -41,6 +43,7 @@ class Tea(HotDrink):
 
 
 class Milk(HotDrink):
+    
     def mix(self):
         print("2. Pass the pressured vapor through the milk")
     
