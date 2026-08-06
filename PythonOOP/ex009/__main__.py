56f423e7fc2aef2541186975abf6de25975961d0
Jean <jeantoneli@gmail.com>
2026-08-06 22:50:51 +0100

    setting up ex009

---
+++ PythonOOP/ex009/__main__.py
@@ -0,0 +1,11 @@
+import ex009
+from rich import print, inspect
+
+
+def main():
+    av1 = ex009.Evaluation("Pedro", "Math", 9.5)
+    inspect(av1)
+
+if __name__ == "__main__":
+    main()
+
---
+++ PythonOOP/ex009/ex009.py
@@ -0,0 +1,8 @@
+
+
+class Evaluation:
+    def __init__(self, name, discipline, grade):
+        self.name = name
+        self.discipline = discipline
+        self.grade = grade
+        
