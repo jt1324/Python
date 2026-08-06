ade24338351ef54a73c5c90d362eb9de4062db4c
Jean <jeantoneli@gmail.com>
2026-08-06 22:33:22 +0100

    protecting variables

--- PythonOOP/ex008/__main__.py
+++ PythonOOP/ex008/__main__.py
@@ -1,7 +1,12 @@
 from ex008 import BankAccount
 
 def main():
-    pass
+    c1 = BankAccount(111, "Mary", 5000)
+    c1.depo(-500)
+    c1.withd(-100)
+    c1.__balance = 0
+
+    print(c1)
 
 if __name__ == "__main__":
     main()
--- PythonOOP/ex008/ex008.py
+++ PythonOOP/ex008/ex008.py
@@ -6,27 +6,34 @@ class BankAccount:
 
   def __init__(self, id, name, balance = 0):
     self.id = id
-    self.owner = name
-    self.balance = balance
-    print (f"Account {self.id} created succesfully. Ballance £{self.balance:,.2f}.")
+    self._owner = name
+    self.__balance = balance
+    print (f"Account {self.id} created succesfully. Ballance £{self.__balance:,.2f}.")
 
   def __str__(self):
-    return f"The account {self.id} from {self.owner} has £{self.balance:,.2f} of balance."
+    # return f"The account {self.id} from {self._owner} has £{self.__balance:,.2f} of balance."
+    return f"Account current status: {self.__dict__}"
 
   def depo(self, amount):
-    self.balance += amount
-    print (f"Deposit of £{amount:,.2f} confirmed to account {self.id}.")
+    if amount < 0:
+      print("You can't add a negative amount")
+    else:
+      self.__balance += amount
+      print (f"Deposit of £{amount:,.2f} confirmed to account {self.id}.")
   
   def withd(self, amount):
-    if amount > self.balance:
-      print(f'Insufficient balance for this withdrwal. Your balnace is £{self.balance:,.2f}.')
+    if amount < 0:
+      print("You can't withdraw a negative amount")
     else:
-      self.balance -= amount
-      print (f"Withdraw of £{amount:,.2f} authorised from account {self.id}.")
+      if amount > self.__balance:
+        print(f'Insufficient balance for this withdrwal. Your balnace is £{self.__balance:,.2f}.')
+      else:
+        self.__balance -= amount
+        print (f"Withdraw of £{amount:,.2f} authorised from account {self.id}.")
 
 
-a1 = BankAccount(112, 'Jean', 3000)
-a1.depo(1500)
-a1.withd(5000)
-print(a1)
+# a1 = BankAccount(112, 'Jean', 3000)
+# a1.depo(1500)
+# a1.withd(5000)
+# print(a1)
 # print(a1.__doc__)
