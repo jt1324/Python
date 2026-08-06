496714efbc449c17d387da2111cd52b557edbc38
Jean <jeantoneli@gmail.com>
2026-08-06 23:06:49 +0100

    assessor methods ex009

--- PythonOOP/ex009/__main__.py
+++ PythonOOP/ex009/__main__.py
@@ -4,7 +4,9 @@ from rich import print, inspect
 
 def main():
     av1 = ex009.Evaluation("Pedro", "Math", 9.5)
-    inspect(av1)
+    print(f"[blue]{av1.name}[/] has grade [green]{av1.get_grade()}[/] in [red]{av1.discipline}[/]")
+    inspect(av1, private=True)
+
 
 if __name__ == "__main__":
     main()
--- PythonOOP/ex009/ex009.py
+++ PythonOOP/ex009/ex009.py
@@ -1,8 +1,18 @@
-
+from rich import print
 
 class Evaluation:
-    def __init__(self, name, discipline, grade):
+    def __init__(self, name, discipline, grade = 0):
         self.name = name
         self.discipline = discipline
-        self.grade = grade
-        
+        self._grade = grade
+
+    # Assessor methods (getters)
+    def get_grade(self):
+        return self._grade
+
+    # Mutator methods (setters)
+    def set_grade(self, value):
+        if 0 <= value <= 10:
+            return self._grade
+        else:
+            print("The grade must be between 0 and 10")
