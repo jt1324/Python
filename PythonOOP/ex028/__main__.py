7d2aa7e031f9a8a214204f24e6b67ae06e95f66b
Jean <jeantoneli@gmail.com>
2026-08-10 22:25:12 +0100

    ex028 final touches

--- PythonOOP/ex028/__main__.py
+++ PythonOOP/ex028/__main__.py
@@ -3,8 +3,8 @@ from rich import print, inspect
 
 def main():
     t = Termostat()
-    # t.temperature = 25
-    # print(f"The current temperature is {t.ftemperature}")
+    t.temperature = 27.2
+    print(f"The current temperature is [green]{t.temperature}[/]")
     inspect(t, private=True, methods=True )
 
 if __name__ == "__main__":
--- PythonOOP/ex028/classes028.py
+++ PythonOOP/ex028/classes028.py
@@ -2,7 +2,8 @@
 
 class Termostat:
     def __init__(self, temperature=24):
-        self.__temperature = temperature
+        self.__temperature = 24
+        self.temperature = temperature
 
     @property
     def temperature(self):
@@ -12,9 +13,10 @@ class Termostat:
     def temperature(self, value):
         if value < 16 or value > 30:
             print(f"{value} is not a valid temperature")
+        elif (value * 2) % 1 != 0:
+            print(f"{value} is not a valid temperature (use whole numbers or .5)")
         else:
             self.__temperature = value
-
 
 
     @property
