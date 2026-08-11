fa96573f0ea2e9896be8c37fbd8c4be9663a83fd
Jean <jeantoneli@gmail.com>
2026-08-11 22:40:15 +0100

    ex032 done

--- PythonOOP/ex032/__main__.py
+++ PythonOOP/ex032/__main__.py
@@ -3,12 +3,11 @@ from rich import print, inspect
 
 def main():
 
-    a1 = BankAccount(112, 'Jean', 3000, "dunha")
+    a1 = BankAccount(112, 'Jean', 3000)
     a1.depo(1500)
     a1.withd(500)
     print(a1)
-# print(a1.__doc__)
-
+    
     # inspect(a1, private=True, methods=True)
 
 
