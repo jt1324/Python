f5da291013f3fce7444fd031a7f12a991e2f0f77
Jean <jeantoneli@gmail.com>
2026-08-11 22:57:59 +0100

    ex032 - name change

--- PythonOOP/ex032/BankAccount.py
+++ PythonOOP/ex032/BankAccount.py
@@ -5,7 +5,7 @@ import pwinput
 class BankAccount:
   def __init__(self, id, name, balance=0, password=None):
     self._id = id
-    self._owner = name
+    self._name = name
     self.__balance = balance
     self.__hash = ""
     if password is None:
@@ -14,7 +14,20 @@ class BankAccount:
     print(f"Account {self._id} created succesfully. Ballance £{self.__balance:,.2f}.")
 
   def __str__(self):
-    return f"The account {self._id} from {self._owner} has £{self.__balance:,.2f} of balance."
+    return f"The account {self._id} from {self._name} has £{self.__balance:,.2f} of balance."
+
+  @property
+  def name(self):
+    return self._name
+
+  @name.setter
+  def name(self, value):
+    ask_password = self.ask_password()
+    if self.password_check(ask_password):
+      self._name = value
+      print(f"Name changed to {value}")
+    else:
+      print("Invalid password")
 
   def depo(self, amount):
     self.__balance += amount
@@ -24,13 +37,13 @@ class BankAccount:
     if password is None:
       password = self.ask_password()
     if self.password_check(password):
-        if amount > self.__balance:
-            print(f'Insufficient balance for this withdrwal. Your balnace is £{self.__balance:,.2f}.')
-        else:
-            self.__balance -= amount
-            print(f"Withdraw of £{amount:,.2f} authorised from account {self._id}.")
+      if amount > self.__balance:
+        print(f'Insufficient balance for this withdrwal. Your balnace is £{self.__balance:,.2f}.')
+      else:
+        self.__balance -= amount
+        print(f"Withdraw of £{amount:,.2f} authorised from account {self._id}.")
     else:
-        print("Invalid password")
+      print("Invalid password")
 
   def password_check(self, password):
     if self.__hash == hashlib.sha256(password.encode()).hexdigest():
--- PythonOOP/ex032/__main__.py
+++ PythonOOP/ex032/__main__.py
@@ -3,11 +3,13 @@ from rich import print, inspect
 
 def main():
 
-    a1 = BankAccount(112, 'Jean', 3000)
+    a1 = BankAccount(112, 'Jean', 5000)
     a1.depo(1500)
     a1.withd(500)
     print(a1)
     
+    a1.name = "John"
+    print(a1)
     # inspect(a1, private=True, methods=True)
 
 
