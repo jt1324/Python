406d1d8b00bc6553a7fd969715a6dc67bb8e6e01
Jean <jeantoneli@gmail.com>
2026-08-13 22:13:14 +0100

    ex016 - polymorphism

---
+++ Exercises/ex116/__main__.py
@@ -0,0 +1,14 @@
+from rich import print
+
+
+print(len("Jean"))
+print(len(["Course", "Python"]))
+print(len({"name": "Jean", "age": 20}))
+
+
+print(+12)
+print(4+8)
+print("poly"+"tech")
+print([1, 2, 3] + [4, 5])
+
+
