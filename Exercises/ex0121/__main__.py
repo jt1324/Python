c85bb0878745981b025f7dc484415c996861a4e7
Jean <jeantoneli@gmail.com>
2026-08-15 21:54:43 +0100

    setting up ex121

---
+++ Exercises/ex0121/__main__.py
@@ -0,0 +1,11 @@
+from classes import *
+from rich import print
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Exercises/ex0121/classes.py
@@ -0,0 +1,16 @@
+from rich import print
+
+
+class Door:
+    def open(self):
+        print("turn the door holder and pull | push")
+
+
+class Company:
+    def open(self):
+        print("Get the documentation to set up a company")
+
+
+class Egg:
+    def open(self):
+        print("Break the shell")
