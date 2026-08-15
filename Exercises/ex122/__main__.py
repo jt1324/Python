fff87591884206447056ce8df49bfee8477ff3a1
Jean <jeantoneli@gmail.com>
2026-08-15 22:09:43 +0100

    setting up ex122

---
+++ Exercises/ex122/__main__.py
@@ -0,0 +1,12 @@
+from classes import *
+from rich import rich
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Exercises/ex122/classes.py
@@ -0,0 +1,38 @@
+from rich import print
+
+class Number:
+    
+    def __init__(self):
+        pass
+
+    def fold(self):
+        pass
+
+
+class Text:
+
+    def __init__(self):
+        pass
+
+    def fold(self):
+        pass
+
+
+class List:
+
+    def __init__(self):
+        pass
+
+    def fold(self):
+        pass
+
+
+class Paper:
+
+    def __init__(self):
+        pass
+
+    def fold(self):
+        pass
+
+    
